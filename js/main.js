c250734d53992b515f133a215a788a9e939bb41a
mattfaucher <mfauch4444@gmail.com>
2021-06-04 12:18:38 -0400

    added links to github and linkedin

--- js/main.js
+++ js/main.js
@@ -16,3 +16,13 @@ if (navClose) {
         navMenu.classList.remove('show-menu');
     })
 }
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
