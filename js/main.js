d48c081e14f1748a8de5bb058163f0ca0c09cd50
mattfaucher <mfauch4444@gmail.com>
2021-06-08 14:12:55 -0400

    progress on footer, social links, adding dark/light mode now

--- js/main.js
+++ js/main.js
@@ -67,3 +67,51 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active');
     });
 });
+
+/* ==== SCROLL SECTIONS ACTIVE LINK ==== */
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+
+/* ==== CHANGE BACKGROUND HEADER ==== */
+function scrollHeader() {
+    const nav = document.getElementById('header');
+
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/* ==== SHOW SCROLL UP ==== */
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 560) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/* ==== DARK MODE/LIGHT MODE ==== */
