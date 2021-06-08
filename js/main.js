30303e3fa83bbac90a7a4d864ba7653f2ed6d8ce
mattfaucher <mfauch4444@gmail.com>
2021-06-08 15:56:11 -0400

    updated media queries for different sized devices

--- js/main.js
+++ js/main.js
@@ -115,3 +115,29 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp);
 
 /* ==== DARK MODE/LIGHT MODE ==== */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// Previously selected topic (if selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// Get current theme that UI has
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// Validate the selected theme and update classList accordingly
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Add event listener for toggle button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // Save user preference
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
