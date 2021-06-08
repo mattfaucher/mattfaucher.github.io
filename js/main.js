50b1d9bfc2f7a5d7a8909ac02c136f4181566f1b
mattfaucher <mfauch4444@gmail.com>
2021-06-08 12:05:33 -0400

    added progress bars to skills, also creating timeline feature for work/education

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-/* MENU SHOW */
+/* ==== MENU SHOW ==== */
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     });
 }
 
-/* MENU HIDDEN */
+/* ==== MENU HIDDEN ==== */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     })
 }
 
-/* REMOVE MENU MOBILE */
+/* ==== REMOVE MENU MOBILE ==== */
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
@@ -26,3 +26,44 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* ==== SKILLS ACCORDION ==== */
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+// Function to toggle the Skills content to be shown or hidden
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((e) => {
+    e.addEventListener('click', toggleSkills);
+});
+
+/* ==== QUALIFICATION TABS ==== */
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        });
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active');
+        });
+        tab.classList.add('qualification__add');
+    });
+});
