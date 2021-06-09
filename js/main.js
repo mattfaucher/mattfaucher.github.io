762f26d63bc484fa74ad0b8dbeeb6ffd1ef2b796
Matt Faucher <mfauch4444@gmail.com>
2021-06-09 12:13:04 -0400

    swiper effectively added just needt to populate with projects and imgs

--- js/main.js
+++ js/main.js
@@ -71,12 +71,15 @@ tabs.forEach(tab => {
 /* ==== PROJECTS SWIPER ==== */
 let swiper = new Swiper(".project__container", {
         cssMode: true,
+        loop: true,
+
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
         pagination: {
           el: ".swiper-pagination",
+          clickable: true,
         },
         mousewheel: true,
         keyboard: true,
